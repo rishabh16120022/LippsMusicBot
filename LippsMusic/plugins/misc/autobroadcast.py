d395ef58bcb3bb54a932459b6f00efad717a2876
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-04-18 11:04:48 +0530

    Update autobroadcast.py

--- LippsMusic/plugins/misc/autobroadcast.py
+++ LippsMusic/plugins/misc/autobroadcast.py
@@ -83,7 +83,7 @@ async def continuous_broadcast():
                 pass
 
         # Wait for 100000 seconds before next broadcast
-        await asyncio.sleep(100000)
+        await asyncio.sleep(10)
 
 # Start the continuous broadcast loop if AUTO_GCAST is True
 if AUTO_GCAST:  
