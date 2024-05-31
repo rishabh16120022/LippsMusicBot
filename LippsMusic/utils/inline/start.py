2ca1dddd0924b2a5910e86046c2fecfd009703d9
rishabh16120022 <rishutripathi161@gmail.com>
2024-05-31 17:06:33 +0530

    start.py

--- LippsMusic/utils/inline/start.py
+++ LippsMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-           InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/Lippsxd/LippsMusicBot"),
+           InlineKeyboardButton(text=_["❤ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❤"], url=f"https://t.me/Niksonfire"),
         ],
     ]
     return buttons
